f7a628ff3e307159ea16d406ff985678d722aac6
Noriaki Mushino <nmushino@redhat.com>
2025-07-12 08:28:52 +0900

    Modified Deserializer

--- src/main/java/io/quarkusdroneshop/reward/infrastructure/OrderTicketDeserializer.java
+++ src/main/java/io/quarkusdroneshop/reward/infrastructure/OrderTicketDeserializer.java
@@ -1,11 +1,20 @@
 package io.quarkusdroneshop.reward.infrastructure;
 
-import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
-import io.quarkusdroneshop.domain.valueobjects.OrderIn;
+import io.quarkusdroneshop.reward.domain.OrderBatch;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.serialization.Deserializer;
+import java.io.IOException;
 
-public class OrderTicketDeserializer extends ObjectMapperDeserializer<OrderIn> {
+public class OrderTicketDeserializer implements Deserializer<OrderBatch> {
 
-    public OrderTicketDeserializer() {
-        super(OrderIn.class);
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public OrderBatch deserialize(String topic, byte[] data) {
+        try {
+            return mapper.readValue(data, OrderBatch.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to deserialize OrderBatch", e);
+        }
     }
 }
